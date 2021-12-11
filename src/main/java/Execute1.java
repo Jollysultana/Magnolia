ece229d0896503ef0f4f497dfab2792d769019db
Jollysultana <salesforcejolly@gmail.com>
2021-12-11 00:35:57 -0500

    Add a new line

---
+++ src/main/java/Execute1.java
@@ -0,0 +1,8 @@
+public class Execute1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("This is practice for Github");
+        
+        
+    }
+}
